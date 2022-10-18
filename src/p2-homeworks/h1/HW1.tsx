98fb13ba3dcc17b2bfd1fb6c2fd60638816775f8
lgnvmaksim <lgnvmaksim@gmail.com>
2022-10-18 10:27:05 +0300

    done HW1 without verstka

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {Message} from "./Message";
 
-
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
 
 function HW1() {
-    const messageData = {
-        avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-        name: 'Some Name',
-        message: 'some text',
-        time: '22:00',
-    }
+
     return (
         <div>
             <hr/>
